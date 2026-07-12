570b7b8fe5c25c45438fd179f82bd8f8578c7b6d
Alex <mark.x1astrastanova@gmail.com>
2026-07-12 07:59:00 +0700

    Update otomatis: Penyesuaian arsitektur GKHV Frontend & Backend

--- run_alert.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-cd /d "x:\Python Project\GKHV"
-python telegram_alert.py
-echo.
-echo ===================================================
-echo Tekan tombol apa saja untuk menutup jendela ini...
-pause >nul
